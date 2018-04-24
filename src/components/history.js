937b097c5a733c2114438926eb0333820985fb07
Will Cheng <willchengsters@gmail.com>
2018-04-24 15:45:14 -0700

    Navbar routing to home, submission, and history pages

---
+++ src/components/history.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+class History extends Component{
+    render(){
+        return(
+            <div>
+                <h1 className="display-4 text-center">Expense History</h1>
+            </div>
+        )
+    }
+}
+
+export default History;
