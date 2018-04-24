b4acdc6922bc1438265c529bc6ae3e00b8f18ab5
Will Cheng <willchengsters@gmail.com>
2018-04-24 16:15:05 -0700

    successfully connected to mongoose database inside server directory

--- server/server.js
+++ server/server.js
@@ -16,4 +16,15 @@ server.use(express.json())
 server.use(express.urlencoded({extended:false}))
 
 
+
+
+
+mongoose.connect(keys.mongoURI, err=>{
+    if(err){
+        throw err;
+    }
+
+    console.log('Successfully connceted to the mLab database!')
+})
+
 server.listen(PORT, ()=> {console.log('server is listening to '+PORT)})
